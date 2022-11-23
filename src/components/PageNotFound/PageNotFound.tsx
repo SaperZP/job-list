9480fb16d8155eb12fc6b8e095a2c10bb0fcfee8
Andrew Bilous <bilous.a.work@gmail.com>
2022-11-23 23:51:22 +0200

    link fix

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -12,17 +12,15 @@ const PageNotFound: FC<PageNotFoundProps> = () => (
         Page Not Found
       </div>
       <button className="mt-5">
-        <a
+        <Link
+        to="/"
             className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
         >
-        <span
-            className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
-        ></span>
-
+        <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
           <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-          <Link to="/">Go Home</Link>
+          Go Home
         </span>
-        </a>
+        </Link>
       </button>
     </main>
 );
